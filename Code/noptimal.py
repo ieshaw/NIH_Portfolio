527e1589f9bf82806d2c122fe6e414e16602839f
Ian Shaw <ianedwardshaw@gmail.com>
2019-05-29 21:35:13 -0400

    Finished finding lags

--- Code/noptimal.py
+++ Code/noptimal.py
@@ -2,39 +2,69 @@ import pandas as pd
 
 class noptimal:
 
-    def __init__(self, data_df):
+    def __init__(self, data_df, isFundingStationary=False, isRateStationary=False):
         '''
         input data_df: pandas dataframe with columns 
             ['disease', 'year', 'funding', 'rate']
             where 'rate' could be the number of YLL or DALY
         '''
         self.data_df = data_df.copy(deep=True)
-        self.diseases = self.data_df['disease'].unique()
-        self.disease_map = {}
-        i = 1
-        self.rate_returns_df = pd.DataFrame(0,index=self.data_df['year'].unique().tolist(),
-                                            columns=[str(i) for i in range(1,len(self.diseases)+1)])
-        #TODO: Error chack have data for each year for each disease
+        self.diseases = self.data_df['disease'].unique().tolist()
+        self.years = self.data_df['year'].unique().tolist()
+        self.rate_returns_df = pd.DataFrame(0,index= self.years,
+                                            columns=[str(i) for i in range(len(self.diseases))])
+        #Error chack have data for each year for each disease
+        year_counts = self.data_df['year'].value_counts()
+        if max(year_counts) != min(year_counts):
+            raise ValueError('''
+                    Not all years appear for each disease.
+                    ''')
         self.funding_returns_df = self.rate_returns_df.copy(deep=True)
+        #Hash map (python dict) diseases in case names have spaces or bad characters
+        i = 0
+        self.disease_map = {}
         for disease in self.diseases:
             self.disease_map[disease] = i
             dis_df = self.data_df.loc[self.data_df['disease'] == disease].copy(deep=True)
             self.rate_returns_df[str(i)] = dis_df['rate'].values
             self.funding_returns_df[str(i)] = dis_df['funding'].values
             i += 1
-
-    #TODO: Turn df's into returns df's
+        if not isFundingStationary:
+            self.funding_returns_df = self.funding_returns_df.pct_change()
+            self.funding_returns_df.dropna(inplace=True)
+        if not isRateStationary:
+            self.rate_returns_df = self.rate_returns_df.pct_change()
+            self.rate_returns_df.dropna(inplace=True)
 
     def find_lag(self):
         diseases = self.data_df['disease'].unique()
-        corr_dict = {}
+        self.lag_dict = {}
         for disease in diseases:
-            corr_dict[disease] = 0
+            self.lag_dict[disease] = {"corr": 0, "lag": 0}
             dis_df = self.data_df.loc[self.data_df['disease'] == disease].copy(deep=True)
-            for i in range(1,dis_df['year'].nunique()):
+            for lag in range(dis_df['year'].nunique() - 1):
                 curr_corr = dis_df['funding'].corr(dis_df['rate'])
-            print(dis_df)
+                if abs(curr_corr) > abs(self.lag_dict[disease]["corr"]):
+                    self.lag_dict[disease]["corr"] = curr_corr
+                    self.lag_dict[disease]["lag"] = lag
+                dis_df['rate'] = dis_df['rate'].shift(-1)
+                dis_df.dropna(inplace=True)
 
+    def lag_adjust_rate_df(self):
+        self.find_lag()
+        for disease_hash in self.rate_returns_df.columns:
+            disease = self.diseases[int(disease_hash)]
+            lag = self.lag_dict[disease]["lag"]
+            if lag < 0:
+                raise ValueError('''
+                        A lag of {} years for the rate of {} 
+                        has a correlation of {} with funding.
+                        This implies funding increases disease frequency.
+                        Cannot find rate correlation between diseases if 
+                        lag correlation coefficient is negative.
+                        '''.format(lag,disease,self.lag_dict[disease]["lag"]))
+            self.rate_returns_df[disease_hash] = self.rate_returns_df[disease_hash].shift(int(-1*lag))
+        self.rate_returns_df.dropna(inplace=True)
 
     def __del__(self):
         pass
--- Code/test.py
+++ Code/test.py
@@ -37,10 +37,50 @@ class Test_noptimal_class(unittest.TestCase):
             self.test_noptimal = noptimal(self.daly_df)
         '''
 
+        def test_missing_year(self):
+            a_list = [['A', 2010, 1, 1],
+                       ['A', 2011, 2, 1], 
+                       ['A', 2012, 3, 2],
+                       ['A', 2013, 4, 3],
+                       ['A', 2015, 5, 4]]
+            b_list = [['B', 2010, 1, 1],
+                       ['B', 2011, 2, 1], 
+                       ['B', 2012, 3, 1],
+                       ['B', 2013, 4, 2],
+                       ['B', 2014, 5, 3]]
+            entry_lists = [a_list, b_list]
+            entries = []
+            for entry_list in entry_lists:
+                entries.extend(entry_list)
+            df_cols = ['disease', 'year', 'funding', 'rate']
+            daly_df = pd.DataFrame(entries, index=None, columns=df_cols)
+            with self.assertRaises(ValueError) as cm:
+                noptimal(daly_df)
+
+        def test_make_funding_stationary(self):
+            self.assertEqual(self.test_noptimal.funding_returns_df.at[2011,"0"],1)
+            self.assertEqual(self.test_noptimal.funding_returns_df.at[2012,"1"],0.5)
+
+        def test_make_rate_stationary(self):
+            self.assertEqual(self.test_noptimal.rate_returns_df.at[2011,"0"],0)
+            self.assertEqual(self.test_noptimal.rate_returns_df.at[2013,"1"],1)
+
         def test_find_lag(self):
             self.test_noptimal.find_lag()
+            self.assertEqual(self.test_noptimal.lag_dict["A"]["lag"], 1)
+            self.assertEqual(self.test_noptimal.lag_dict["B"]["lag"], 2)
 
+        def test_lag_adjust_rate_df(self):
+            self.test_noptimal.lag_adjust_rate_df()
+            shape = self.test_noptimal.rate_returns_df.shape
+            self.assertEqual(shape[0], 2)            
+            self.assertEqual(shape[1], 2)            
         
+        #TODO: Make This Test
+        def test_lag_adjust_rate_df_negative_corr(self):
+            pass
+
+        #TODO: Try the lag adjust rate df function on our date
 
 
 if __name__ == '__main__':
